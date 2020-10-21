99fd4b062caf3def01c53d3c07e5c61e839dfb46
lvbing <lvbingwm@gmail.com>
2020-10-21 11:59:12 +0800

    min-stack

---
+++ leetcode/editor/cn/[155]最小栈.py
@@ -0,0 +1,80 @@
+# 设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。 
+# 
+#  
+#  push(x) —— 将元素 x 推入栈中。 
+#  pop() —— 删除栈顶的元素。 
+#  top() —— 获取栈顶元素。 
+#  getMin() —— 检索栈中的最小元素。 
+#  
+# 
+#  
+# 
+#  示例: 
+# 
+#  输入：
+# ["MinStack","push","push","push","getMin","pop","top","getMin"]
+# [[],[-2],[0],[-3],[],[],[],[]]
+# 
+# 输出：
+# [null,null,null,null,-3,null,0,-2]
+# 
+# 解释：
+# MinStack minStack = new MinStack();
+# minStack.push(-2);
+# minStack.push(0);
+# minStack.push(-3);
+# minStack.getMin();   --> 返回 -3.
+# minStack.pop();
+# minStack.top();      --> 返回 0.
+# minStack.getMin();   --> 返回 -2.
+#  
+# 
+#  
+# 
+#  提示： 
+# 
+#  
+#  pop、top 和 getMin 操作总是在 非空栈 上调用。 
+#  
+#  Related Topics 栈 设计 
+#  👍 700 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+import math
+
+
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.min = math.inf
+
+    def push(self, x: int) -> None:
+        if self.min >= x:
+            self.stack.append(self.min)
+            self.min = x
+
+        self.stack.append(x)
+
+    def pop(self) -> None:
+        if self.stack.pop() == self.min:
+            self.min = self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+# leetcode submit region end(Prohibit modification and deletion)
